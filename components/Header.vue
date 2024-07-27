0c46beb1a0e3e3f86e16c0df2e8db7bba88f10ff
RizniRajbiaa <152113269+RizniRajbiaa@users.noreply.github.com>
2024-07-27 12:08:58 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="row p-3 bg-primary text-white">
+    <div class="col-ig-1">
+      <img src="~/assets/img/LOGO-SMK4.png" alt="logo SMKN4" width="100"> 
+    </div>
+
+  <div class="col-ig-11">
+    <h3>Perpus Digital</h3>
+    <address>
+      SMKN 4 TASIKMALAYA
+      Jl.Depok, Sukamenak, Purbaratu
+    </address>
+  </div>
+  </div>
+</template>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2/family=poppins:wght@400;500;900&display=swap");
+@import url("https://fonts.googleapis.com/css2/family=Libre+Baskerville:wght@4700&family=poppins:wght@400;500;900&display=swap");
+
+h2 {
+  font-family: "franklin gothic medium", "arial narrow", arial, sans-serif;
+  color: rgb(244, 239, 239);
+}
+h4,
+h6 {
+  color: #615c5c;
+}
+
+.addres {
+  color: #9b8b8b;
+}
+
+nav {
+  color: #2353b4;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="container-fluid">
+    <Header />  <!--custom component-->
+
+    <slot />   <!-- page akan mengisi pada bagian ini-->
+  </div>
+</template>
